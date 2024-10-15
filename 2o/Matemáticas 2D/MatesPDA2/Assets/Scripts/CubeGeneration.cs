8bdfb9a68ce1ac370ea9f908b581f1390e7a3199
theflash48 <59892110+theflash48@users.noreply.github.com>
2024-10-15 11:13:09 +0200

    a

--- 2o/Matemáticas 2D/MatesPDA2/Assets/Scripts/CubeGeneration.cs
+++ 2o/Matemáticas 2D/MatesPDA2/Assets/Scripts/CubeGeneration.cs
@@ -4,54 +4,91 @@ using UnityEngine;
 
 public class CubeGeneration : MonoBehaviour
 {
-    public int width = 256;
-    public int height = 256;
-    public float scale = 20f;
-    public float offsetX = 100f;
-    public float offsetY = 100f;
+    public int width = 10;
+    public int height = 10;
 
     public float multiplicadorDesplazamiento;
     public float velocidadCubo;
+    float timer = 0f;
 
     public GameObject cube;
+    public GameObject powerUp;
+    public GameObject torreta;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        GenerateTexture();
+
+        cube.SetActive(false);
+        if (timer <= 0f)
+        {
+            GenerateObjects();
+        }
+        GeneratorTimer();
+
+
     }
 
-    void GenerateTexture()
+    void GeneratorTimer()
     {
+        timer -= Time.deltaTime;
+        Debug.Log(timer);
+    }
+
+    void GenerateObjects()
+    {
+        
         float perlinHeight;
-        for (int x = 0; x < width; x++)
+        for (float x = -4.5f; x < 5.5; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                perlinHeight = CalcularColor(x, y);
-                if (perlinHeight < 0.4)
+                perlinHeight = CalcularAltura(x, y);
+                if (perlinHeight < 0.04)
                 {
-                    
+                    Vector3 pos = new Vector3(x, 0.5f, 10);
+                    Instantiate<GameObject>(cube, pos, Quaternion.identity);
+                }
+                else
+                {
+                    GenerarTorreta(x);
                 }
             }
         }
-        
+        timer = 5;
     }
 
-    float CalcularColor(int x, int y)
+    void GenerarTorreta(float x)
+    {
+        int value = Random.Range(0, 101);
+        if (value < 5)
+        {
+            Vector3 pos = new Vector3(x, 0.5f, 10);
+            Instantiate<GameObject>(torreta, pos, Quaternion.identity);
+        }
+        if (value > 95)
+        {
+            Vector3 pos = new Vector3(x, 0.5f, 10);
+            Instantiate<GameObject>(powerUp, pos, Quaternion.identity);
+        }
+    }
+
+    float CalcularAltura(float x, int y)
     {
 
-        float xCoord = (float)x / width * scale + Random.Range(0, 999999f);
-        float yCoord = (float)y / height * scale + Random.Range(0, 999999f);
+        float xCoord = (float)x / 1 + Random.Range(0, 999999f);
+        float yCoord = (float)y / 1 + Random.Range(0, 999999f);
 
         float value = Mathf.PerlinNoise(xCoord, yCoord);
 
         return value;
     }
+
+    
 }
