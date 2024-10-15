3d5ad93c2a3dad50498e0164b58587cec7f224d0
theflash48 <pabloski.2002@gmail.com>
2024-10-15 20:27:22 +0200

    a

---
+++ 2o/Matemáticas 2D/MatesPDA2/Assets/Scripts/ObjectGeneration.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGeneration : MonoBehaviour
+{
+    public float multiplicadorDesplazamiento;
+    public float velocidadCubo;
+    float timer = 0f;
+
+    public GameObject cube;
+    public GameObject powerUp;
+    public GameObject tanque;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0f)
+        {
+            timer = 5f;
+            GenerateObjects();
+        }
+        GeneratorTimer();
+    }
+
+    void GeneratorTimer()
+    {
+        timer -= Time.deltaTime;
+    }
+
+    void GenerateObjects()
+    {
+        float perlinHeight;
+        for (float x = -4.5f; x < 5.5; x++)
+        {
+            perlinHeight = CalcularAltura(x, 10);
+            if (perlinHeight < 0.4)
+            {
+                Vector3 pos = new Vector3(x, 0.5f, 10);
+                Instantiate<GameObject>(cube, pos, Quaternion.identity);
+            }
+            else
+            {
+                GenerarExtras(x);
+            }
+        }
+    }
+
+    void GenerarExtras(float x)
+    {
+        float value = Random.Range(0f, 100f);
+        Debug.Log(value);
+        if (value <= 5)
+        {
+            Vector3 pos = new Vector3(x, 0.5f, 10);
+            Instantiate<GameObject>(tanque, pos, Quaternion.identity);
+        }
+        if (value >= 95)
+        {
+            Vector3 pos = new Vector3(x, 0.5f, 10);
+            Instantiate<GameObject>(powerUp, pos, Quaternion.identity);
+        }
+    }
+
+    float CalcularAltura(float x, int y)
+    {
+        float xCoord = (float)x / 1000 * Random.Range(0, 999999f);
+        float yCoord = (float)y / 1000 * Random.Range(0, 999999f);
+
+        float value = Mathf.PerlinNoise(xCoord, yCoord);
+
+        return value;
+    }
+
+    
+}
--- 2o/Matemáticas 2D/MatesPDA2/Assets/Scripts/ObjectMovement.cs
+++ 2o/Matemáticas 2D/MatesPDA2/Assets/Scripts/ObjectMovement.cs
@@ -2,14 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeMovement : MonoBehaviour
+public class ObjectMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
