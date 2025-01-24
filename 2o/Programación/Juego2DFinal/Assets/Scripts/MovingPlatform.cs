d608b04475293e26d4026a639303da86aafaa3b7
theflash48 <59892110+theflash48@users.noreply.github.com>
2025-01-25 00:54:19 +0100

    a

---
+++ 2o/Programación/Juego2DFinal/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public float speed;
+    public float desplazamiento;
+    public float posicion;
+    public bool horizontal = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (horizontal)
+        {
+            posicion = transform.position.x;
+        }
+        else
+        {
+            posicion = transform.position.y;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (horizontal)
+        {
+            
+            if ((posicion + desplazamiento) <= transform.position.x)
+            {
+                speed *= -1;
+            }
+            if ((posicion - desplazamiento) >= transform.position.x)
+            {
+                speed *= -1;
+            }
+            transform.Translate(speed * Time.deltaTime * Vector2.right);
+        }
+        else
+        {
+            if ((posicion + desplazamiento) <= transform.position.x)
+            {
+                speed *= -1;
+            }
+            if ((posicion - desplazamiento) >= transform.position.x)
+            {
+                speed *= -1;
+            }
+            transform.Translate(speed * Time.deltaTime * Vector2.up);
+        }
+    }
+}
