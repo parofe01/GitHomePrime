4ba4d6b0265b53fe91fa84046e3a43f70c16a15b
theflash48 <59892110+theflash48@users.noreply.github.com>
2025-02-04 13:25:24 +0100

    a

---
+++ 2o/Fisicas/PracticasDeFisicas/Assets/Scripts/Ejercicios1.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Ejercicios1 : MonoBehaviour
+{
+
+    public enum Exercice { Uno, Dos, Tres, Cuatro, Cinco, Seis, Siete, Ocho }
+    public Exercice ex;
+
+    // Inputs
+    float _IAxisH;
+    float _IAxisV;
+    bool _IJump;
+
+    // Components
+    Rigidbody _Rigidbody;
+
+    // Variables
+    public float _vForce;
+    public bool _vGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+    }
+
+    void FixedUpdate()
+    {
+        PhisicsMachine();
+    }
+
+    void Inputs()
+    {
+        _IAxisH = Input.GetAxis("Horizontal");
+        _IAxisV = Input.GetAxis("Vertical");
+        _IJump = Input.GetButtonDown("Jump");
+    }
+    void PhisicsMachine()
+    {
+        switch (ex)
+        {
+            case Exercice.Uno: Uno(); break;
+            case Exercice.Dos: Dos(); break;
+            case Exercice.Tres: Tres(); break;
+            case Exercice.Cuatro: Cuatro(); break;
+            case Exercice.Cinco: Cinco(); break;
+            case Exercice.Seis: Seis(); break;
+            case Exercice.Siete: Siete(); break;
+
+        }
+    }
+
+    void Uno()
+    {
+        if(_IAxisV > 0)
+        {
+            _Rigidbody.AddForce(Vector3.forward * _vForce, ForceMode.Force);
+        }
+    }
+
+    void Dos()
+    {
+        if (_IAxisV != 0)
+        {
+            _Rigidbody.AddForce(Vector3.forward * _IAxisV * _vForce, ForceMode.Force);
+        }
+        if (_IAxisH != 0)
+        {
+            _Rigidbody.AddForce(Vector3.right * _IAxisH * _vForce, ForceMode.Force);
+        }
+    }
+    void Tres()
+    {
+        if ( _IAxisV > 0)
+        {
+            _Rigidbody.AddForce(Vector3.forward * _vForce, ForceMode.Force);
+        }
+        if (_IAxisV < 0)
+        {
+            _Rigidbody.AddForce(Vector3.back * _vForce/2, ForceMode.Force);
+        }
+    }
+    void Cuatro()
+    {
+        if (_IAxisV != 0)
+        {
+            _Rigidbody.velocity = Vector3.forward * _IAxisV * _vForce;
+        }
+        if (_IAxisH != 0)
+        {
+            _Rigidbody.velocity = Vector3.right * _IAxisH * _vForce;
+        }
+    }
+    void Cinco()
+    {
+        if (_IJump && _vGrounded)
+        {
+            _Rigidbody.AddForce(Vector3.up * _vForce, ForceMode.Impulse);
+        }
+    }
+    void Seis()
+    {
+
+    }
+    void Siete()
+    {
+
+    }
+
+    void Ocho()
+    {
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            _vGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            _vGrounded = false;
+        }
+    }
+}
