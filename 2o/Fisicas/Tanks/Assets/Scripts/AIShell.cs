222a7114e233447ffbc1b48aff04e6c08ff9d9f5
theflash48 <59892110+theflash48@users.noreply.github.com>
2025-03-11 11:16:53 +0100

    a

---
+++ 2o/Fisicas/Tanks/Assets/Scripts/AIShell.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AIShell : MonoBehaviour
+{
+    public GameObject explosion;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.tag == "tank") {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+}
