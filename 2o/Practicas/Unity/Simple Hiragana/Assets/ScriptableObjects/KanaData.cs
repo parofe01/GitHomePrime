97e5ee9cb1cd442fb10050aaaab85dfcd35cab44
theflash48 <59892110+theflash48@users.noreply.github.com>
2025-05-20 22:19:28 +0200

    a

---
+++ 2o/Practicas/Unity/Simple Hiragana/Assets/ScriptableObjects/KanaData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KanaData", menuName = "Scriptable Objects/KanaData")]
+public class KanaData : ScriptableObject
+{
+    
+}
---
+++ 2o/Practicas/Unity/Simple Hiragana/Assets/ScriptableObjects/KanaDatabase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KanaDatabase", menuName = "Scriptable Objects/KanaDatabase")]
+public class KanaDatabase : ScriptableObject
+{
+    
+}
