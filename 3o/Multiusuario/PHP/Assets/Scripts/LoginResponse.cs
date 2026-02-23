61335a93e331c78f7943509eff0c5021e12fc633
theflash48 <59892110+theflash48@users.noreply.github.com>
2026-02-23 01:09:36 +0100

    a

--- 3o/Multiusuario/PHP/Assets/Scripts/LoginResponse.cs
+++
@@ -1,6 +0,0 @@
-[System.Serializable]
-public class LoginResponse
-{
-    public string status;
-    public int id;   // vendrá del PHP cuando el login sea correcto
-}
--- 3o/Shaders/intro webgl/fondo_color.js
+++
@@ -1,5 +0,0 @@
-// especifica en RGBA el color de fondo (4 valores entre 0 y 1)
-gl.clearColor (1.0,0.0,0.0,1.0) ;
-// rellena el buffer de color utilizando el color
-// especificado con la orden clearColor
- gl.clear(gl .COLOR_BUFFER_BIT);
